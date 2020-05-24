0a7398b580aaeccd2cc78b1dc37aff5036c520ad
Jacob Shiohira <jlshiohira23@gmail.com>
2020-05-24 17:06:02 -0500

    Make basic stylistic changes, revise out of date dependencies

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
-"""
-In this file, we'll create a routing layer to handle incoming and outgoing
-requests between our bot and Slack.
-"""
+""" basic routing layer to handle incoming and outgoing requests between our bot and slack """
 import json
 import jinja2
 from flask import render_template, request
 from slackeventsapi import SlackEventAdapter
 from bot import Bot
+from match import generate_matches
 
-
-mybot = Bot()
-events_adapter = SlackEventAdapter(mybot.verification, "/slack")
+matching_bot = Bot()
+events_adapter = SlackEventAdapter(matching_bot.verification, "/slack")
 
 template_loader = jinja2.ChoiceLoader([
                     events_adapter.server.jinja_loader,
@@ -22,47 +19,55 @@ events_adapter.server.jinja_loader = template_loader
 
 @events_adapter.server.route("/install", methods=["GET"])
 def before_install():
-    """
-    This route renders an installation page for our app!
-    """
-    client_id = mybot.oauth["client_id"]
+    """ renders an installation page for our app """
+    client_id = matching_bot.oauth["client_id"]
     return render_template("install.html", client_id=client_id)
 
 
 @events_adapter.server.route("/thanks", methods=["GET"])
 def thanks():
-    """
-    This route renders a page to thank users for installing our app!
-    """
+    """ this route renders a page to thank users for installing our app """
     auth_code = request.args.get('code')
-
-    # Now that we have a classy new Bot Class, let's build and use an auth
-    # method for authentication.
-    mybot.auth(auth_code)
+    matching_bot.auth(auth_code)
     return render_template("thanks.html")
 
 
-# Let's add an event handler for messages coming into our bot's test channel
-@events_adapter.on("message")
-def handle_message(event_data):
-    """
-    Here we'll build a 'message' event handler using the Slack Events Adapter.
-    """
-    pass
-
-
-# Here's some helpful debugging hints for checking that env vars are set
+# here's some helpful debugging hints for checking that env vars are set
 @events_adapter.server.before_first_request
 def before_first_request():
-    client_id = mybot.oauth.get("client_id")
-    client_secret = mybot.oauth.get("client_secret")
-    verification = mybot.verification
+    client_id = matching_bot.oauth.get("client_id")
+    client_secret = matching_bot.oauth.get("client_secret")
+    verification = matching_bot.verification
     if not client_id:
-        print "Can't find Client ID, did you set this env variable?"
+        print("Can't find Client ID, did you set this env variable?")
     if not client_secret:
-        print "Can't find Client Secret, did you set this env variable?"
+        print("Can't find Client Secret, did you set this env variable?")
     if not verification:
-        print "Can't find Verification Token, did you set this env variable?"
+        print("Can't find Verification Token, did you set this env variable?")
+
+
+@events_adapter.server.route("/slack/generatematches", methods=["POST"])
+def handle_generatematches_command():
+    """ this route renders a page to thank users for installing our app """
+    data = request.data
+    # TODO: can i check to see who initiated the slack command from the payload?
+    print(data)
+    return "OK", 200
+
+
+# let's add an event handler for messages coming into our bot's test channel
+# using the slack events adapter, when we receive a message event
+@events_adapter.on("message")
+def handle_message(event_data):
+    # grab the message from the event payload
+    message = event_data["event"]
+    # if the user says hello
+    if "hello" in message.get('text'):
+        # have our bot respond to the message
+        matching_bot.say_hello(message)
+    else:
+        # otherwise help us find out what went wrong
+        print("This isn't the message we expected: \n{}\n".format(message))
 
 
 if __name__ == '__main__':
--- bot.py
+++ bot.py
@@ -1,37 +1,38 @@
 # -*- coding: utf-8 -*-
-"""
-In this file, we'll create a python Bot Class.
-"""
 import os
 
 from slackclient import SlackClient
 
-
 class Bot(object):
-    """ Instanciates a Bot object to handle Slack interactions."""
     def __init__(self):
         super(Bot, self).__init__()
-        # When we instantiate a new bot object, we can access the app
+        # when we instantiate a new bot object, we can access the app
         # credentials we set earlier in our local development environment.
         self.oauth = {"client_id": os.environ.get("CLIENT_ID"),
                       "client_secret": os.environ.get("CLIENT_SECRET"),
-                      # Scopes provide and limit permissions to what our app
-                      # can access. It's important to use the most restricted
+                      # scopes provide and limit permissions to what our app
+                      # can access. it's important to use the most restricted
                       # scope that your app will need.
                       "scope": "bot"}
         self.verification = os.environ.get("VERIFICATION_TOKEN")
         self.client = SlackClient("")
 
     def auth(self, code):
+        """ here we'll create a method to exchange the temporary auth code for an
+        oauth token and save it in memory on our bot object for easier access.
         """
-        Here we'll create a method to exchange the temporary auth code for an
-        OAuth token and save it in memory on our Bot object for easier access.
-        """
-        pass
+        auth_response = self.client.api_call("oauth.access",
+                                             client_id=self.oauth['client_id'],
+                                             client_secret=self.oauth['client_secret'],
+                                             code=code)
+        # we'll save the bot_user_id to check incoming messages mentioning our bot
+        self.bot_user_id = auth_response["bot"]["bot_user_id"]
+        self.client = SlackClient(auth_response["bot"]["bot_access_token"])
 
     def say_hello(self, message):
-        """
-        Here we'll create a method to respond when a user DM's our bot
-        to say hello!
-        """
-        pass
+        """ here we'll create a method to respond when a user dm's our bot to say hello! """
+        channel = message["channel"]
+        hello_response = "I want to live! :pray: Please build me <@%s>" % message["user"]
+        self.client.api_call("chat.postMessage",
+                             channel=channel,
+                             text=hello_response)
