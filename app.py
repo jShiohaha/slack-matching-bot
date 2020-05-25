062341f7a626b11f4c41ddfc1a19d18e9af20c9b
Jacob Shiohira <jlshiohira23@gmail.com>
2020-05-25 14:28:13 -0500

    Update slash route to handle (un)authorized users and slash command resonse

--- app.py
+++ app.py
@@ -3,15 +3,19 @@
 import json
 
 # external package imports
-import jinja2
 from flask import render_template, request
+import jinja2
+from slack import WebClient
 from slackeventsapi import SlackEventAdapter
 
 # local project imports 
 from src.bot import Bot
+from src.store import RaikesMatchBotClient
 from src.match import generate_matches
 
-matching_bot = Bot()
+# if this becomes more complex, we can use dependency injection
+mongo_client = RaikesMatchBotClient()
+matching_bot = Bot(mongo_client)
 events_adapter = SlackEventAdapter(matching_bot.verification, "/slack")
 
 template_loader = jinja2.ChoiceLoader([
@@ -20,13 +24,11 @@ template_loader = jinja2.ChoiceLoader([
                   ])
 events_adapter.server.jinja_loader = template_loader
 
-
 @events_adapter.server.route("/install", methods=["GET"])
 def before_install():
     """ renders an installation page for our app """
     client_id = matching_bot.oauth["client_id"]
     return render_template("install.html", client_id=client_id)
-
 
 @events_adapter.server.route("/thanks", methods=["GET"])
 def thanks():
@@ -34,7 +36,6 @@ def thanks():
     auth_code = request.args.get('code')
     matching_bot.auth(auth_code)
     return render_template("thanks.html")
-
 
 # here's some helpful debugging hints for checking that env vars are set
 @events_adapter.server.before_first_request
@@ -49,30 +50,39 @@ def before_first_request():
     if not verification:
         print("Can't find Verification Token, did you set this env variable?")
 
-
 @events_adapter.server.route("/slack/generatematches", methods=["POST"])
-def handle_generatematches_command():
-    """ this route renders a page to thank users for installing our app """
-    data = request.data
-    # TODO: can i check to see who initiated the slack command from the payload?
-    print(data)
-    return "OK", 200
-
-
-# let's add an event handler for messages coming into our bot's test channel
-# using the slack events adapter, when we receive a message event
-@events_adapter.on("message")
-def handle_message(event_data):
-    # grab the message from the event payload
-    message = event_data["event"]
-    # if the user says hello
-    if "hello" in message.get('text'):
-        # have our bot respond to the message
-        matching_bot.say_hello(message)
+def handle_generate_matches_command():
+    channel_id = request.form.get('channel_id', None)
+    user_id = request.form.get('user_id', None)
+    response = ""
+    if user_id not in mongo_client.get_admin_users():
+        # send unauthorized message
+        response = ("Sorry, only specified users are authorized generate matches. "
+                    "If you think you should be authorized, reach out to Anna or Mark.")
     else:
-        # otherwise help us find out what went wrong
-        print("This isn't the message we expected: \n{}\n".format(message))
-
+        response = matching_bot.generate_matches(channel_id)
+    return {
+	    "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": response
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Notice an issue or bug? Submit an issue <https://github.com/jShiohaha/slack-matching-bot/issues|here>."
+                }
+            },
+        ],
+        "response_type": "in_channel"
+    }, 200
 
 if __name__ == '__main__':
     events_adapter.start(debug=True)
